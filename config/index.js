1f1a34c78f4111d57a19a551683a6751ae644875
Jose Cuevas <josecueram@gmail.com>
2020-05-13 19:49:59 -0500

    created mongo Lib

--- config/index.js
+++ config/index.js
@@ -2,7 +2,14 @@ require('dotenv').config()
 
 const config = {
   dev: process.env.NODE_ENV !== 'production',
-  port: process.env.PORT || 3000
+  port: process.env.PORT || 3000,
+  cors: process.env.CORS,
+  mongo: {
+    user: process.env.DB_USER,
+    name: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST
+  }
 }
 
 module.exports = config
