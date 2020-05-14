111ee2cfe6669ff28d6ecf0243496aa0528ec86d
Jose Cuevas <josecueram@gmail.com>
2020-05-13 23:41:28 -0500

    created notFoundHandler using @hapi/boom

--- index.js
+++ index.js
@@ -3,12 +3,17 @@ const moviesApi = require('./routes/movies')
 const PORT = require('./config').port
 
 const { errorHandler, errorWrapper, errorLogger } = require('./utils/middlewares/errors')
+const notFoundHandler = require('./utils/middlewares/notFoundHandler')
 
 const app = express()
 
 app.use(express.json())
 
+// ? Set movies routes
 moviesApi(app)
+
+// ? Handle 404 Not Found
+app.use(notFoundHandler)
 
 // ? Error Handling Middlewares
 app.use(errorLogger)
